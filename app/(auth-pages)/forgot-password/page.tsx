a9ee55ed3a80bddf26967d139447da387157e8da
Franciszek Czajka <68653056+fczajka@users.noreply.github.com>
2025-07-15 12:57:53 +0200

    Merge Setup project to Main (#1)

---
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1>Forgot Password works</h1>
+    </div>
+  );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile works</h1>
+    </div>
+  );
+}
