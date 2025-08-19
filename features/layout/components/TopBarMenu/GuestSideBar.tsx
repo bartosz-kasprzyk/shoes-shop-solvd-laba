c9f2b9fd614dbf1e5bf265c1e9ccd65f9fb9a6cc
Franciszek Czajka <68653056+fczajka@users.noreply.github.com>
2025-08-19 09:47:13 +0200

    Fix padding in user dialog and dropdown (#69)
    * Fix padding in user dialog and dropdown
    
    * Remove disablePadding

--- features/layout/components/TopBarMenu/GuestSideBar.tsx
+++ features/layout/components/TopBarMenu/GuestSideBar.tsx
@@ -11,6 +11,7 @@ export default function GuestSideBar() {
         alignItems: 'center',
         justifyContent: 'center',
         textAlign: 'center',
+        paddingTop: 1,
       }}
     >
       <Typography
