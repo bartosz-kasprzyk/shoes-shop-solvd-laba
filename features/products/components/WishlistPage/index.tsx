ae769995d29f4453f92685b13b4c6ed5923b2334
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2025-08-19 02:17:52 +0200

    add 'My Wishlist' header

--- features/products/components/WishlistPage/index.tsx
+++ features/products/components/WishlistPage/index.tsx
@@ -28,55 +28,80 @@ export default function WishlistPage() {
 
   if (wishlist.length === 0) {
     return (
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center',
-          paddingTop: { xs: '100px', md: '200px' },
-          margin: '0 40px',
-        }}
-      >
-        <Box
-          sx={{
-            width: 64,
-            height: 64,
-            backgroundColor: '#f3f4f6',
-            borderRadius: '50%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: 3,
-          }}
-        >
-          <MyWishlistIcon />
-        </Box>
+      <Box sx={{ flex: 1, padding: { xs: '20px 30px', md: '40px 60px' } }}>
         <Typography
-          variant='h6'
+          variant='h4'
+          component='h2'
           sx={{
             fontWeight: 600,
-            marginBottom: 3,
+            marginBottom: 6,
             color: '#1f2937',
-            fontSize: { xs: 16, lg: 22 },
+            fontSize: { xs: 35, lg: 42 },
           }}
         >
-          You don&apos;t have any products in your wishlist yet
+          My Wishlist
         </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            paddingTop: { xs: '50px', md: '150px' },
+          }}
+        >
+          <Box
+            sx={{
+              width: 64,
+              height: 64,
+              backgroundColor: '#f3f4f6',
+              borderRadius: '50%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginBottom: 3,
+            }}
+          >
+            <MyWishlistIcon />
+          </Box>
+          <Typography
+            variant='h6'
+            sx={{
+              fontWeight: 600,
+              marginBottom: 3,
+              color: '#1f2937',
+              fontSize: { xs: 16, lg: 22 },
+            }}
+          >
+            You don&apos;t have any products in your wishlist yet
+          </Typography>
 
-        <Link href='/products' passHref>
-          <Button variant='primary' sx={{ p: 1, px: 2 }}>
-            Browse products
-          </Button>
-        </Link>
+          <Link href='/products' passHref>
+            <Button variant='primary' sx={{ p: 1, px: 2 }}>
+              Browse products
+            </Button>
+          </Link>
+        </Box>
       </Box>
     );
   }
 
   return (
     <ScrollableContainer>
-      <Box px={2}>
+      <Box sx={{ flex: 1, padding: { xs: '20px 30px', md: '40px 60px' } }}>
+        <Typography
+          variant='h4'
+          component='h2'
+          sx={{
+            fontWeight: 600,
+            marginBottom: 6,
+            color: '#1f2937',
+            fontSize: { xs: 35, lg: 42 },
+          }}
+        >
+          My Wishlist
+        </Typography>
         <Grid container spacing={2}>
           {wishlist.map((product) => (
             <Grid key={product.id} size={{ xs: 6, md: 6, lg: 4, xl: 3 }}>
