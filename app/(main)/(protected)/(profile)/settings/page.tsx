5397a485f26d0001887e2a0520d860234b488cd8
PeEpEGa <106858735+PeEpEGa@users.noreply.github.com>
2025-08-19 13:40:39 +0200

    Minor fixes to update profile (#66)
    * minor fixes to update profile
    
    * fix styling

--- app/(main)/(protected)/(profile)/settings/page.tsx
+++ app/(main)/(protected)/(profile)/settings/page.tsx
@@ -5,7 +5,15 @@ import { Box } from '@mui/material';
 export default function ProfilePage() {
   return (
     <ScrollableContainer>
-      <Box sx={{ px: '60px', py: '50px' }}>
+      <Box
+        sx={{
+          px: { xs: 2, lg: '60px' },
+          py: {
+            xs: 'none',
+            lg: '24px',
+          },
+        }}
+      >
         <h1>My Profile</h1>
         <ProfileForm />
       </Box>
