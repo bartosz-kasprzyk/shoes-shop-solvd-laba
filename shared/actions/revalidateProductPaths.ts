d04a7bea2041d970ff529e375e96e4bfba58faed
szolkiewicz <s.d.zolkiewicz@gmail.com>
2025-08-28 23:56:06 +0200

    fix: important fix for revalidate products

--- shared/actions/revalidateProductPaths.ts
+++ shared/actions/revalidateProductPaths.ts
@@ -2,7 +2,7 @@
 import { revalidatePath } from 'next/cache';
 
 export async function revalidateProductPaths(id?: number) {
-  revalidatePath('/products');
+  revalidatePath('/products', 'layout'); //very important dont delete!
   if (id) {
     revalidatePath(`/product/${id}`);
   }
