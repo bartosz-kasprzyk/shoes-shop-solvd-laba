da9636990a8079cdb3ca00a8aeecc3b52b6afd55
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-02-27 17:44:42 +0100

    definitive patch for CVE-2025-66478

--- shared/components/ui/Button/interface.ts
+++ shared/components/ui/Button/interface.ts
@@ -1,7 +1,9 @@
 import { type ButtonProps } from '@mui/material';
 
-export interface CustomButtonProps
-  extends Omit<ButtonProps, 'onClick' | 'variant'> {
+export interface CustomButtonProps extends Omit<
+  ButtonProps,
+  'onClick' | 'variant'
+> {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   variant?: 'primary' | 'outline' | 'outline-black';
 }
