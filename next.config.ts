2b0eee23f42567ecf79c3899dc1e14ad58c56e59
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-02-27 17:26:14 +0100

    patch CVE-2025-66478 with Next 16.0.10

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   async redirects() {
     return [
       {
